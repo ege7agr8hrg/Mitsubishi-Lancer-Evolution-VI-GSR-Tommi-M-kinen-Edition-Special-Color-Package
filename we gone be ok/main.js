53a24e5e9eb174db5d20c98c64bf5e16264efacd
ege7agr8hrg <legiaphucachau@gmail.com>
2026-01-25 22:39:39 +0700

    Add navigation functionality and new pages for account, products, and service

---
+++ we gone be ok/main.js
@@ -0,0 +1,19 @@
+function inputproducts() {
+    window.location.href = "products.html";
+}
+
+function inputaccount() {
+    window.location.href = "account.html";
+}
+
+// function inputcart() {
+//     window.location.href = "cart.html";
+// }
+
+function inputservice() {
+    window.location.href = "service.html";
+}
+
+function inputhome() {
+    window.location.href = "Main.html";
+}
